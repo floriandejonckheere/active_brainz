0caff4e31240c498a1ef1aabed3506cbf80d84e5
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-06-28 13:13:34 +0200

    Initial commit

---
+++ lib/active_brainz.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "active_brainz/version"
+
+module ActiveBrainz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/active_brainz_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe ActiveBrainz do
+  it "has a version number" do
+    expect(ActiveBrainz::VERSION).not_to be nil
+  end
+end
