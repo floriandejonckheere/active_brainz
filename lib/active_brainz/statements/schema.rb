e5f661df88126d34442d12463d396421a456a4f1
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-08-03 21:42:19 +0200

    Skip entity references

--- lib/active_brainz/statements/schema.rb
+++ lib/active_brainz/statements/schema.rb
@@ -33,6 +33,7 @@ module ActiveBrainz
 
       def add_foreign_key(from_table, to_table, **options)
         return unless Database::TABLES.include?(from_table) && Database::TABLES.include?(to_table)
+        return if %w(entity0 entity1).include? options[:column]
 
         tables[from_table].references << Reference.new(from_table, to_table, :belongs_to, options)
         tables[to_table].references << Reference.new(to_table, from_table, :has_many, options)
