2d0329b4f17f899fbe8e95d9b550a839af07e97d
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-02-16 19:24:57 +0100

    Add schema version and date

--- lib/active_brainz/version.rb
+++ lib/active_brainz/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "date"
+
 module ActiveBrainz
   module Version
     MAJOR = 0
@@ -11,6 +13,12 @@ module ActiveBrainz
 
     STRING = [VERSION, PRE].compact.join("-")
   end
+
+  # Version of MusicBrainz' SQL schema
+  SCHEMA_VERSION = 25
+
+  # Publication date of MusicBrainz' SQL schema
+  SCHEMA_DATE = Date.new(2021, 2, 13)
 
   VERSION = ActiveBrainz::Version::STRING
 end
