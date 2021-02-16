7127b05530037a7be3ffe2e39c9e7bfbe517932a
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-02-16 21:23:31 +0100

    Update database table list to latest version

--- lib/active_brainz/database.rb
+++ lib/active_brainz/database.rb
@@ -13,56 +13,104 @@ module ActiveBrainz
 
     # Processed tables
     TABLES = %w(
-      alternative_medium
-      alternative_medium_track
       alternative_release
       alternative_release_type
+      alternative_medium
       alternative_track
+      alternative_medium_track
       annotation
       application
+      area_type
       area
-      area_alias
+      area_gid_redirect
       area_alias_type
+      area_alias
       area_annotation
-      area_attribute
       area_attribute_type
       area_attribute_type_allowed_value
-      area_gid_redirect
+      area_attribute
       area_tag
       area_tag_raw
-      area_type
       artist
-      artist_alias
       artist_alias_type
+      artist_alias
       artist_annotation
-      artist_attribute
       artist_attribute_type
       artist_attribute_type_allowed_value
-      artist_credit
-      artist_credit_name
-      artist_gid_redirect
+      artist_attribute
       artist_ipi
       artist_isni
       artist_meta
-      artist_rating_raw
       artist_tag
+      artist_rating_raw
       artist_tag_raw
+      artist_credit
+      artist_credit_name
+      artist_gid_redirect
       artist_type
+      autoeditor_election
+      autoeditor_election_vote
+      cdtoc
+      cdtoc_raw
       country_area
+      deleted_entity
+      edit
+      edit_data
+      edit_note
+      edit_note_recipient
+      edit_area
+      edit_artist
+      edit_event
+      edit_instrument
+      edit_label
+      edit_place
+      edit_release
+      edit_release_group
+      edit_recording
+      edit_series
+      edit_work
+      edit_url
+      editor
+      old_editor_name
+      editor_language
+      editor_preference
+      editor_subscribe_artist
+      editor_subscribe_artist_deleted
+      editor_subscribe_collection
+      editor_subscribe_label
+      editor_subscribe_label_deleted
+      editor_subscribe_editor
+      editor_subscribe_series
+      editor_subscribe_series_deleted
+      event
+      event_meta
+      event_rating_raw
+      event_tag_raw
+      event_alias_type
+      event_alias
+      event_annotation
+      event_attribute_type
+      event_attribute_type_allowed_value
+      event_attribute
+      event_gid_redirect
+      event_tag
+      event_type
+      release_first_release_date
+      recording_first_release_date
       gender
       genre
       genre_alias
+      instrument_type
       instrument
-      instrument_alias
+      instrument_gid_redirect
       instrument_alias_type
+      instrument_alias
       instrument_annotation
-      instrument_attribute
       instrument_attribute_type
       instrument_attribute_type_allowed_value
-      instrument_gid_redirect
+      instrument_attribute
       instrument_tag
       instrument_tag_raw
-      instrument_type
       iso_3166_1
       iso_3166_2
       iso_3166_3
@@ -101,6 +149,7 @@ module ActiveBrainz
       l_event_series
       l_event_url
       l_event_work
+      l_label_label
       l_instrument_instrument
       l_instrument_label
       l_instrument_place
@@ -110,7 +159,6 @@ module ActiveBrainz
       l_instrument_series
       l_instrument_url
       l_instrument_work
-      l_label_label
       l_label_place
       l_label_recording
       l_label_release
@@ -131,15 +179,15 @@ module ActiveBrainz
       l_recording_series
       l_recording_url
       l_recording_work
-      l_release_group_release_group
-      l_release_group_series
-      l_release_group_url
-      l_release_group_work
       l_release_release
       l_release_release_group
       l_release_series
       l_release_url
       l_release_work
+      l_release_group_release_group
+      l_release_group_series
+      l_release_group_url
+      l_release_group_work
       l_series_series
       l_series_url
       l_series_work
@@ -147,131 +195,158 @@ module ActiveBrainz
       l_url_work
       l_work_work
       label
-      label_alias
+      label_rating_raw
+      label_tag_raw
       label_alias_type
+      label_alias
       label_annotation
-      label_attribute
       label_attribute_type
       label_attribute_type_allowed_value
-      label_gid_redirect
+      label_attribute
       label_ipi
       label_isni
       label_meta
-      label_rating_raw
+      label_gid_redirect
       label_tag
-      label_tag_raw
       label_type
       language
       link
       link_attribute
-      link_attribute_credit
-      link_attribute_text_value
       link_attribute_type
       link_creditable_attribute_type
+      link_attribute_credit
       link_text_attribute_type
+      link_attribute_text_value
       link_type
       link_type_attribute_type
+      editor_collection
+      editor_collection_type
+      editor_collection_collaborator
+      editor_collection_area
+      editor_collection_artist
+      editor_collection_event
+      editor_collection_instrument
+      editor_collection_label
+      editor_collection_place
+      editor_collection_recording
+      editor_collection_release
+      editor_collection_release_group
+      editor_collection_series
+      editor_collection_work
+      editor_collection_deleted_entity
+      editor_oauth_token
+      editor_watch_preferences
+      editor_watch_artist
+      editor_watch_release_group_type
+      editor_watch_release_status
       medium
-      medium_attribute
       medium_attribute_type
       medium_attribute_type_allowed_format
       medium_attribute_type_allowed_value
       medium_attribute_type_allowed_value_allowed_format
+      medium_attribute
       medium_cdtoc
       medium_format
-      medium_index
       orderable_link_type
       place
       place_alias
       place_alias_type
       place_annotation
-      place_attribute
       place_attribute_type
       place_attribute_type_allowed_value
+      place_attribute
       place_gid_redirect
       place_tag
       place_tag_raw
       place_type
+      replication_control
       recording
-      recording_alias
       recording_alias_type
+      recording_alias
+      recording_rating_raw
+      recording_tag_raw
       recording_annotation
-      recording_attribute
       recording_attribute_type
       recording_attribute_type_allowed_value
-      recording_gid_redirect
+      recording_attribute
       recording_meta
-      recording_rating_raw
+      recording_gid_redirect
       recording_tag
-      recording_tag_raw
       release
-      release_alias
       release_alias_type
+      release_alias
+      release_country
+      release_unknown_country
+      release_raw
+      release_tag_raw
       release_annotation
-      release_attribute
       release_attribute_type
       release_attribute_type_allowed_value
-      release_country
-      release_coverart
+      release_attribute
       release_gid_redirect
-      release_group
-      release_group_alias
-      release_group_alias_type
-      release_group_annotation
-      release_group_attribute
-      release_group_attribute_type
-      release_group_attribute_type_allowed_value
-      release_group_gid_redirect
-      release_group_meta
-      release_group_primary_type
-      release_group_rating_raw
-      release_group_secondary_type
-      release_group_secondary_type_join
-      release_group_tag
-      release_group_tag_raw
-      release_label
       release_meta
+      release_coverart
+      release_label
       release_packaging
-      release_raw
       release_status
       release_tag
-      release_tag_raw
-      release_unknown_country
+      release_group
+      release_group_alias_type
+      release_group_alias
+      release_group_rating_raw
+      release_group_tag_raw
+      release_group_annotation
+      release_group_attribute_type
+      release_group_attribute_type_allowed_value
+      release_group_attribute
+      release_group_gid_redirect
+      release_group_meta
+      release_group_tag
+      release_group_primary_type
+      release_group_secondary_type
+      release_group_secondary_type_join
       script
       series
-      series_alias
+      series_type
+      series_ordering_type
+      series_gid_redirect
       series_alias_type
+      series_alias
       series_annotation
-      series_attribute
       series_attribute_type
       series_attribute_type_allowed_value
-      series_gid_redirect
-      series_ordering_type
+      series_attribute
       series_tag
       series_tag_raw
-      series_type
       tag
       tag_relation
       track
       track_gid_redirect
       track_raw
+      medium_index
       url
       url_gid_redirect
       vote
       work
-      work_alias
+      work_language
+      work_rating_raw
+      work_tag_raw
       work_alias_type
+      work_alias
       work_annotation
-      work_attribute
+      work_gid_redirect
+      work_meta
+      work_tag
+      work_type
       work_attribute_type
       work_attribute_type_allowed_value
-      work_gid_redirect
-      work_language
-      work_meta
-      work_rating_raw
-      work_tag
-      work_tag_raw
-      work_type
+      work_attribute
+      release_event
+      event_series
+      recording_series
+      release_series
+      release_group_series
+      work_series
     ).freeze
 
     class << self
