05987d8cc3f24520474612b5f18209165c478247
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-02-19 12:35:51 +0100

    Render HasBeginEndDate concern

--- lib/active_brainz/statements/table_binding.rb
+++ lib/active_brainz/statements/table_binding.rb
@@ -5,6 +5,16 @@ require "delegate"
 module ActiveBrainz
   module Statements
     class TableBinding < SimpleDelegator
+      FILTERED_ATTRIBUTES = %w(
+        begin_date_year
+        begin_date_month
+        begin_date_day
+        end_date_year
+        end_date_month
+        end_date_day
+        gid
+      ).freeze
+
       def render_binding
         binding
       end
@@ -19,6 +29,15 @@ module ActiveBrainz
 
       def gid?
         attributes.any? { |attribute| attribute.name == "gid" }
+      end
+
+      def begin_end_date?
+        %w(begin_date_year begin_date_month begin_date_day end_date_year end_date_month end_date_day)
+          .all? { |name| attributes.any? { |attribute| attribute.name == name } }
+      end
+
+      def filtered_attributes
+        attributes.reject { |attr| FILTERED_ATTRIBUTES.include? attr.name }
       end
     end
   end
